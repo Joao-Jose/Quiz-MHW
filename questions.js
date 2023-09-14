a4a214f4c85fbe596c6570e2d54f531698d640f0
João <joaojose.neto4560@gmail.com>
2023-09-14 11:39:58 -0300

    adição de mais perguntas

--- questions.js
+++ questions.js
@@ -17,25 +17,25 @@ export default [
     },
 
     {
-        question: "Qual monstro é conhecido por sua resistência ao gelo?",
+        question: "A Joia desafiante é um adorno que fornece qual habilidade ?",
         answers: [
-            { option: "(A) Rathalos", correct: false },
-            { option: "(B) Diablos", correct: true },
-            { option: "(C) Uragan", correct: false },
+            { option: "(A) Reforço de Ataque", correct: false },
+            { option: "(B) Desempenho Maximo", correct: false },
+            { option: "(C) Agitador", correct: true },
         ],
     },
 
     {
-        question: "Qual é o proximo da lista ? Rathalos, Legiana, Barioth...",
+        question: "Quantas armas existem em Monster Hunter World: Iceborne ?",
         answers: [
-            { option: "(A) Tigrex ", correct: true },
-            { option: "(B) Yian Garuga ", correct: false },
-            { option: "(C) Pukei-Pukei Coralino ", correct: false },
+            { option: "(A) 10 ", correct: false },
+            { option: "(B) 11 ", correct: false },
+            { option: "(C) 12 ", correct: true },
         ],
     },
 
     {
-        question: "Na missão de high rank 'Redefinindo o Casal poderoso' você enfrenta quais Monstros ?",
+        question: "Na missão de high rank ''Redefinindo o Casal poderoso'' você enfrenta quais Monstros ?",
         answers: [
             { option: "(A) Diablos e Diablos Negra", correct: false },
             { option: "(B) Rathalos e Rathian", correct: true },
@@ -44,7 +44,7 @@ export default [
     },
 
     {
-        question: "Qual adorno é resposável por esta habilidade: 'Aljava' ?",
+        question: "A Joia Aljava é um adorno que fornece qual habilidade ?",
         answers: [
             { option: "(A) Reforço de capacidade", correct: true },
             { option: "(B) Muro de Ferro", correct: false },
@@ -53,12 +53,146 @@ export default [
     },
 
     {
-        question: "o Impacto de tal contra-golpe neutraliza empurrões que você recebe, esta frase descreve qual Arma ?",
+        question: "Quais itens usamos para a criação da Mega poção ?",
         answers: [
-            { option:"(A) Lâmina Dínamo", correct: false },
-            { option:"(B) Lançarma", correct: false },
-            { option:"(C) Lança", correct: false },
-            { option:"(D) Espadão", correct: true },
-        ]
-    }
-]
+            { option:"(A) Mel e Poção ancestral", correct: false },
+            { option:"(B) Mel e Poção Máxima", correct: false },
+            { option:"(C) Mel e Mega Nutrientes", correct: false },
+            { option:"(D) Mel e Poção", correct: true },
+        ],
+    },
+
+    {
+        question: "O que é necessário para obter o olho de Fatalis ?",
+        answers: [
+            { option:"(A) Apenas matá-lo", correct: false },
+            { option:"(B) É necessario repetição pois a chance de vir depende da quantidade de abates", correct: false },
+            { option:"(C) Quebrar os Chifres por Completo", correct: true },
+            { option:"(D) Utilizar o Manto bandido para ter mais chances de drop", correct: false },
+        ],
+    },
+
+    {
+        question: "A habilidade da cantina ''Resistência elemental'' vem de qual igrediente ?",
+        answers: [
+            { option:"(A) Carnes", correct: false },
+            { option:"(B) Peixes", correct: false },
+            { option:"(C) Vegetais", correct: true },
+        ],
+    },
+
+    {
+        question: "Quantas vezes foi necessário salvar a maldita da Assistente ?",
+        answers: [
+            { option:"(A) Não sei peri as contas", correct: true },
+            { option:"(B) E é possível contar ?", correct: true },
+            { option:"(C) Durante a vida toda", correct: true },
+            { option:"(D) Umas 5, 6 ou 7 sla", correct: true },
+        ],
+    },
+
+    {
+        question: "Quantos colabs tem em Monster Hunter Iceborne ?",
+        answers: [
+            { option:"(A) 5", correct: false },
+            { option:"(B) 6", correct: false },
+            { option:"(C) 7", correct: true },
+            { option:"(D) 8", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual Monstro foi lançado junto a Safi'Jiiva ?",
+        answers: [
+            { option:"(A) Mel e Poção ancestral", correct: false },
+            { option:"(B) Mel e Poção Máxima", correct: false },
+            { option:"(C) Mel e Mega Nutrientes", correct: false },
+            { option:"(D) Zinogre Tenebroso", correct: true },
+        ],
+    },
+
+    {
+        question: "Quantos Monstros existem no jogo base ?",
+        answers: [
+            { option:"(A) 26", correct: false },
+            { option:"(B) 27", correct: false },
+            { option:"(C) 28", correct: false },
+            { option:"(D) 29", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual o Método é utilizado par adiquirir vale gourmet ?",
+        answers: [
+            { option:"(A) Missões da arena da área de encontro e missões de evento de farm de naco de dragoveio", correct: true },
+            { option:"(B) Ajudando players com o nível mais baixo que o seu", correct: false },
+            { option:"(C) Farmando nas terras guias", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual Dragão é o mais temido até mesmo por outros dragões anciões ? ",
+        answers: [
+            { option:"(A) Alatreon", correct: false },
+            { option:"(B) Zora Magdaros", correct: false },
+            { option:"(C) Shara Ishivalda", correct: false },
+            { option:"(D) Nergigante", correct: false },
+            { option:"(E) Fatalis", correct: true },
+        ],
+    },
+
+    {
+        question: "Como se chama a arma que se assemelha a um instrumento ? ",
+        answers: [
+            { option:"(A) Hunting Ornn", correct: false },
+            { option:"(B) Hunting Gore", correct: false },
+            { option:"(C) Hunting Porn", correct: false },
+            { option:"(D) Hunting More", correct: false },
+            { option:"(E) Hunting Horn", correct: true },
+        ],
+    },
+
+    {
+        question: "Como se descreveria o Gore Magala ? ",
+        answers: [
+            { option:"(A) Gole Minhagala", correct: true },
+            { option:"(B) Gore Minhavara", correct: false },
+            { option:"(C) Gore Impala", correct: false },
+            { option:"(D) Morre evaidevala", correct: false },
+            { option:"(E) Não é um dragão ancião", correct: false},
+        ],
+    },
+
+    {
+        question: "Qual monstro recebe mais dano em MHW:I ? ",
+        answers: [
+            { option:"(A) Kulu ya ku", correct: false },
+            { option:"(B) TziTzi ya ku", correct: false },
+            { option:"(C) Pukei Pukei", correct: false },
+            { option:"(D) Paolumu", correct: false },
+            { option:"(E) Dodogama", correct: true },
+        ],
+    },
+
+    {
+        question: "Qual dragão ancião é responsável por sua rápida regeneração ? ",
+        answers: [
+            { option:"(A) Nergigante", correct: true },
+            { option:"(B) Lunastra", correct: false },
+            { option:"(C) Teostra", correct: false },
+            { option:"(D) Kushala", correct: false },
+            { option:"(E) Velkhana", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual dragão ancião é responsável por ser considerado ''sem elemento'' ? ",
+        answers: [
+            { option:"(A) Nergigante", correct: false },
+            { option:"(B) Lunastra", correct: false },
+            { option:"(C) Teostra", correct: false },
+            { option:"(D) Kushala", correct: true },
+            { option:"(E) Velkhana", correct: false },
+        ],
+    },
+]   
