768eff498f865cc08b31e90efff4dc9e71cca282
João <joaojose.neto4560@gmail.com>
2023-09-13 21:27:19 -0300

    quiz-mhw

---
+++ questions.js
@@ -0,0 +1,64 @@
+export default [
+    {
+      question: "Qual item arremessável é necessário para cegar um monstro?",
+      answers: [
+        { option: "(A) Lampejo Inseto", correct: true },
+        { option: "(B) Cápsula de Lampejo", correct: false }, // Correção aqui: "Cápsula" em vez de "Capsula"
+        { option: "(C) Mosclarão", correct: false }, // Correção aqui: "Moscarão" em vez de "Mosclarão"
+      ],
+    },
+    {
+      question: "Quais itens usamos para a criação de poção máxima?",
+      answers: [
+        { option: "(A) Mandrágora e Amarguinseto", correct: false },
+        { option: "(B) Mandrágora e Poção de vida", correct: false },
+        { option: "(C) Mandrágora e Mega Nutrientes", correct: true },
+      ],
+    },
+
+    {
+        question: "Qual monstro é conhecido por sua resistência ao gelo?",
+        answers: [
+            { option: "(A) Rathalos", correct: false },
+            { option: "(B) Diablos", correct: true },
+            { option: "(C) Uragan", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual é o proximo da lista ? Rathalos, Legiana, Barioth...",
+        answers: [
+            { options: "(A) Tigrex ", correct: true },
+            { options: "(A) Yian Garuga ", correct: false },
+            { options: "(A) Pukei-Pukei Coralino ", correct: false },
+        ],
+    },
+
+    {
+        question: "Na missão de high rank 'Redefinindo o Casal poderoso' você enfrenta quais Monstros ?",
+        answers: [
+            { option: "(A) Diablos e Diablos Negra", correct: false },
+            { option: "(A) Rathalos e Rathian", correct: true },
+            { option: "(A) Teostra e Lunastra", correct: false },
+        ],
+    },
+
+    {
+        question: "Qual adorno é resposável por esta habilidade: 'Aljava' ?",
+        answers: [
+            { option: "(A) Reforço de capacidade", correct: true },
+            { option: "(A) Muro de Ferro", correct: false },
+            { option: "(A) Artilharia", correct: false },
+        ],
+    },
+
+    {
+        question: "o Impacto de tal contra-golpe neutraliza empurrões que você recebe, esta frase descreve qual Arma ?",
+        answers: [
+            { option:"(A) Lâmina Dínamo", correct: false },
+            { option:"(A) Lançarma", correct: false },
+            { option:"(A) Lança", correct: false },
+            { option:"(A) Espadão", correct: true },
+        ]
+    }
+]
